47de5f369daaee2970b4a31b71d451dbd2884f5b
Moritz Schaller <mschaller@smart-digital.at>
2023-09-23 12:56:35 +0200

    search added

--- myfind.cpp
+++ myfind.cpp
@@ -1,4 +1,4 @@
-
+#include <limits.h>
 #include <iostream>
 #include <string.h>
 #include <list>
@@ -7,12 +7,53 @@
 #include <unistd.h>
 #include <list>
 #include <string.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+void printFound(std::string fileName, std::string directoryName)
+{
+    std::cout << (int)getpid() << ": " << fileName << ": " << directoryName << (directoryName.back() == '/' ? "" : "/") << fileName << std::endl;
+}
+
+void search(char *directoryName, std::string fileName, bool caseInSensitive)
+{
+    DIR *dirp = opendir(directoryName);
+    if (dirp == NULL)
+    {
+        perror("Failed to get current working directory");
+        exit(1);
+    }
+    struct dirent *direntp = readdir(dirp);
+
+    while (direntp != NULL)
+    {
+
+        direntp = readdir(dirp);
+        char *currentName = direntp->d_name;
+
+        if (caseInSensitive && strcasecmp(currentName, fileName.c_str()) == 0)
+        {
+            // Pass in the current Name because the search string could have upper cases
+            printFound(currentName, directoryName);
+            break;
+        }
+        if (strcmp(currentName, fileName.c_str()) == 0)
+        {
+            printFound(fileName, directoryName);
+            break;
+        }
+    }
+    while ((closedir(dirp) == -1) && (errno == EINTR))
+        ;
+}
 
 int main(int argc, char *argv[])
 {
 
     // Arugment passing
     int opt;
+    int status;
     bool recursive = false;
     bool caseInsensitive = false;
     std::list<std::string> filenames;
@@ -38,8 +79,8 @@ int main(int argc, char *argv[])
         std::cout << "Error: searchpath and filename(s) are required." << std::endl;
         return 1;
     }
+    char *searchPath = argv[optind];
     optind++;
-    std::string searchPath = argv[optind];
     if (optind >= argc)
     {
         std::cout << "Error: at least one filename is required." << std::endl;
@@ -49,18 +90,16 @@ int main(int argc, char *argv[])
     std::cout << "Recursive: " << (recursive ? "true" : "false") << std::endl;
     std::cout << "Case Insensitive: " << (caseInsensitive ? "true" : "false") << std::endl;
     std::cout << "Search Path: " << searchPath << std::endl;
-    while (optind < argc - 1)
+    while (optind < argc)
     {
-        optind++;
         std::string filename(argv[optind]);
-        std::cout << "Filename " << filename << std::endl;
         filenames.push_back(filename);
+        optind++;
     }
     pid_t pid;
 
     for (const auto &filename : filenames)
     {
-        std::cout << "Filename: " << filename << std::endl;
         pid = fork();
         switch (pid)
         {
@@ -68,11 +107,16 @@ int main(int argc, char *argv[])
             fprintf(stderr, "myfork: error when forking child process\n");
             return EXIT_FAILURE;
         case 0: /* child process */
-            printf("--- Child process with PID %d\n", (int)getpid());
+            if (recursive)
+            {
+            }
+            else
+            {
+                search(searchPath, filename, caseInsensitive);
+            }
             return 3;
         default: /* parent */
-            printf("Parent process with PID %d\n", (int)getpid());
-            wiat
+            wait(&status);
         }
     }
 }
